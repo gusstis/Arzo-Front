39332bfbb8e41a558fde48e0548e88c2bab004e2
gussti <gusaranciba@gmail.com>
2022-11-02 18:13:56 -0300

    agregados: /components/Header.jsx y LoginPage.jsx, /dashboard/index.jsx

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,19 @@
+import Image from 'next/image';
 export default function about() {
     return (
-        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-            // eslint-disable-next-line react/no-unescaped-entities
-            <picture className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2>
-            srcSet="./public/beams.webp" type="image/webp"
-                alt=""
-                width=1308
-                height=500
+        <div className = "relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+            <picture>
+            <source srcSet="./public/beams.webp" type="image/webp"/>
+            <img src="./public/beams.webp" alt="imagin"/>
             </picture>
         <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
         <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
           <div className="mx-auto max-w-md">
-            <Image> src="/public/logo-tailwind.svg" className="h-6" alt="Tailwind Play" </Image>
-            <div className="divide-y divide-gray-300/50">
+          <picture>
+            <source srcSet="./public/logo-tailwind.svg" type="image/webp"/>
+            <img src="./public/beams.webp" alt="imaginn"/>
+            </picture>
+             <div className="divide-y divide-gray-300/50">
               <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
                 <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
                 <ul className="space-y-4">
